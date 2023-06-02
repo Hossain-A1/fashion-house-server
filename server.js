95125c5df923553f8e85c3264292eca0f2fc6483
Hossain-A1 <mrhossainahmed7@gmail.com>
2023-06-02 21:45:31 +0300

    mongoDB connected

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-require("dotenv").config()
-const express = require("express")
-const itemsRoutes= require("./routes/itemsRoutes")
- /* express app*/
- const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const itemsRoutes = require("./routes/itemsRoutes");
+/* express app*/
+const app = express();
+const port = process.env.PORT || 4000;
 
 /*middlewares */
-app.use(express.json())
-app.use((req,res,next)=>{
-console.log(req.path, req.method);
-next()
-})
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 /* routes */
-app.use("/api/items", itemsRoutes) 
+app.use("/api/items", itemsRoutes);
 
-
-
- const port = process.env.PORT || 4000
-/* express app*/
- app.listen(port,()=>{
-  console.log(`app listen on port ${port}`);
- })
+/*listen app with mongoDB*/
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`mongoDB connectd and app listen on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
